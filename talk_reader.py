a6d150d2c438aa90fd96372901700a7bcccab6c2
Llohann Dallagnol Speranca <speranca.ai@gmail.com>
2024-11-03 20:02:59 +0530

    changed the process from paragraph-wise to full-text

--- talk_reader.py
+++ talk_reader.py
@@ -60,6 +60,7 @@ class TalkReader:
         self.metadata_file_path = metadata_file_path
         self.talks = []
         self.save_folder = save_folder
+        os.makedirs(save_folder, exist_ok=True)
         self.metadata_fields = [
             "Date",
             "Title",
@@ -68,42 +69,61 @@ class TalkReader:
             "Talks status",
             "Number of words",
             "Talk Language",
+            "Comments",
         ]
         self.parsing_that_went_wrong = []
         self.paragraphs_do_not_starting_with_hashtag = []
         if not os.path.exists(metadata_file_path):
+            os.makedirs(os.path.dirname(metadata_file_path), exist_ok=True)
             with open(self.metadata_file_path, "w") as metadata_file:
                 csv_writer = DictWriter(metadata_file, fieldnames=self.metadata_fields)
                 csv_writer.writeheader()
 
         # self.talks = self.process_document()
 
-    def get_all_text(self):
+    def _get_all_text(self):
         document_text = [p.text for p in self.document.paragraphs]
         document_text = "\n\n".join(document_text)
         return document_text
 
-    def process_document(self):
-        # document = [p.text for p in document.paragraphs]
-        talk = []
-        if os.getenv("DEBUG") == "true":
-            paragraphs = self.document.paragraphs[:1000]
-        else:
-            paragraphs = self.document.paragraphs
+    def _split_text(self, text: str):
+        split_text = [t.strip().removeprefix("\u2003") for t in text.split("##")]
+        split_text = [t for t in split_text if t]
+        split_text = [t.split("\n\n") for t in split_text]
+        return split_text
 
-        for paragraph in tqdm(paragraphs):
-            text: str = paragraph.text
-            if text.startswith("##"):
-                self.add_talk_to_database(talk)
-                text = text.removeprefix("##").removeprefix("\u2003").strip()
-                if text:
-                    talk = [text]
-            # elif '##' in text:
-            #     self.paragraphs_do_not_starting_with_hashtag.append(text)
-            else:
-                talk.append(text)
-        self.add_talk_to_database(talk)
+    def _add_talks_to_database(self, talks: list[list[str]]):
+        for talk in tqdm(talks):
+            self.add_talk_to_database(talk)
+
+    def process_document(self):
+        text = self._get_all_text()
+        talks = self._split_text(text)
+        self._add_talks_to_database(talks)
+        self.dump_metadata()
         return self.metadata
+
+    # def process_document(self):
+    #     # document = [p.text for p in document.paragraphs]
+    #     talk = []
+    #     if os.getenv("DEBUG") == "true":
+    #         paragraphs = self.document.paragraphs[:1000]
+    #     else:
+    #         paragraphs = self.document.paragraphs
+
+    #     for paragraph in tqdm(paragraphs):
+    #         text: str = paragraph.text
+    #         if text.startswith("##"):
+    #             self.add_talk_to_database(talk)
+    #             text = text.removeprefix("##").removeprefix("\u2003").strip()
+    #             if text:
+    #                 talk = [text]
+    #         # elif '##' in text:
+    #         #     self.paragraphs_do_not_starting_with_hashtag.append(text)
+    #         else:
+    #             talk.append(text)
+    #     self.add_talk_to_database(talk)
+    #     return self.metadata
 
     def add_talk_to_database(self, talk):
         try:
@@ -142,19 +162,33 @@ class TalkReader:
         for paragraph in talk:
             _ = document.add_paragraph(paragraph)
             # para.style = document.styles['Normal']
-        try:
-            date_iso = datetime.strptime(metadata["Date"], "%d %B %Y").strftime(
-                "%Y-%m-%d"
-            )
-        except ValueError:
-            date_iso = datetime.strptime(metadata["Date"], "%B %d %Y").strftime(
-                "%Y-%m-%d"
-            )
-        save_file_name = f"{date_iso} {metadata['Title']}, {metadata['Location']}.docx"
+        date_iso = metadata["Date"]
+        save_file_name = (
+            f"{date_iso} – {metadata['Title']} – {metadata['Location']}.docx"
+        )
         document.save(os.path.join(save_folder, save_file_name))
 
     @staticmethod
-    def parse_metadata_from_talk(talk: list[str]):
+    def _extract_date(date_string: str) -> tuple[str, str]:
+        date_string = date_string.replace("Date Unknown", "1 January 0001")
+        date_string = re.sub(
+            "[^A-Za-z0-9 ]|1001|th", "", date_string.strip().replace("  ", " ")
+        )
+        date_string = date_string.replace("Octobre", "October")
+        date_string = date_string.replace("199\n", "0000")
+        date_string = date_string.replace("200\n", "0000")
+        date_string = date_string.replace("Year Unknown", "0000")
+        try:
+            return datetime.strptime(date_string, "%d %B %Y").strftime("%Y-%m-%d"), ""
+        except ValueError:
+            try:
+                return datetime.strptime(date_string, "%B %d %Y").strftime(
+                    "%Y-%m-%d"
+                ), ""
+            except ValueError:
+                return date_string, "Date format not recognized"
+
+    def parse_metadata_from_talk(self, talk: list[str]):
         # [Title, Type of talk, Location, Talks status, Number of words]
         # Example talk:
         # 04 May 1982
@@ -167,15 +201,6 @@ class TalkReader:
         metadata_list = talk[:5]
         number_of_words = sum([len(paragraph.split(" ")) for paragraph in talk])
         metadata = {
-            "Date": re.sub(
-                "[^A-Za-z0-9 ]",
-                "",
-                metadata_list[0]
-                .strip()
-                .replace("th", "")
-                .replace("  ", " ")
-                .replace("1001", ""),
-            ),
             "Title": metadata_list[1],
             "Type of talk": metadata_list[2],
             "Location": metadata_list[3],
@@ -187,6 +212,10 @@ class TalkReader:
             .removeprefix("Transcript: "),
             "Number of words": number_of_words,
         }
+
+        date_iso, comments = self._extract_date(metadata_list[0])
+        metadata["Date"] = date_iso
+        metadata["Comments"] = comments
         return metadata
 
     def dump_metadata(self):
